5416eaa353f30aa535cd547bc0dca8f1c6267342
SkyRed003 <francescopalumbo574@gmail.com>
2024-03-25 12:19:12 +0100

    trampolino boh

--- Assets/Script/PlatformController.cs
+++ Assets/Script/PlatformController.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class PlatformController : MonoBehaviour
 {
   [SerializeField] private float force;
-  [SerializeField] private Animator myAnimationController;
+  [SerializeField] public Animator myAnimationController;
 
   private void OnCollisionEnter2D(Collision2D col) {
 
     if(col.transform.CompareTag("Player")) {
         var player = col.transform.GetComponent<PlayerController>();
         player.Jump(force);
-    } else { 
+    }
+    
+    if(col.transform.CompareTag("Player")) { 
         myAnimationController.SetBool("salto", true);
 
-    }
 
  
+      }
   }
-     private void OnCollisionExit2D (Collision2D col) {
-        if(col.transform.CompareTag("Player")) {
-            myAnimationController.SetBool("salto", false);
-        }
-    }
+
+  private void OnCollisionExit2D(Collision2D col) {
+     if(col.transform.CompareTag("Player")) { 
+        myAnimationController.SetBool("salto", false);
+
+
+ 
+      }
+
+  }
 }
